da96153ab30a864ef56e2a479d5a897fa91e6ecf
SavanRajyaguru <65443618+SavanRajyaguru@users.noreply.github.com>
2022-10-09 09:48:45 +0530

    first commit

---
+++ Basic-Problems/Fib_Number.java
@@ -0,0 +1,37 @@
+//Fib Numbers: 0 1 1 2 3 5 8
+public class Fib_Number {
+
+    // Recursion 
+    // time: O(2^n)
+    // Auxiliary space: O(n)
+    protected static int fib(int n){
+        
+        // base case
+        if(n == 0 || n == 1){
+            return n;
+        }
+        
+        return fib(n-1) + fib(n-2);
+    }
+
+    // Loop DP consept
+    // time: O(n)
+    // Auxiliary space: O(1)
+    protected static void fibonacci(int n){
+        int a = 0;
+        int b = 1;
+        System.out.print(a + " "+ b);
+        for(int i = 2; i <= n; i++){
+            int c = a + b;
+            System.out.print(" "+c);
+            a = b;
+            b = c;
+        }
+    }
+
+    public static void main(String[] args){
+        int n = 9;
+        System.out.println(fib(n));
+        fibonacci(n);
+    } 
+}
---
+++ Basic-Problems/SumOfNumber.java
@@ -0,0 +1,20 @@
+// Time: O(n)
+// Space: O(n)
+
+public class SumOfNumber{
+
+    protected static int sumOfNumber(int n){
+
+        // base case
+        if(n <= 0)
+            return 0;
+            
+        return n + sumOfNumber(n-1);
+    } 
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(sumOfNumber(n));
+    }
+
+}
